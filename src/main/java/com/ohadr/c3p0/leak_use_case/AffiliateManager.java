efdac4fcbe03c86257e5e0e6df6e54f19fddac81
Ohad Redlich <oredlich@group.williamhill.plc>
2017-03-02 14:10:53 +0200

    hibernate to pom + hibernate changes = Reader changes

--- src/main/java/com/ohadr/c3p0/leak_use_case/AffiliateManager.java
+++ src/main/java/com/ohadr/c3p0/leak_use_case/AffiliateManager.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import com.ohadr.c3p0.leak_use_case.entities.AffiliateCampaignEntity;
 import com.ohadr.c3p0.leak_use_case.entities.AffiliateEntity;
 import com.ohadr.c3p0.leak_use_case.entities.CampaignEntity;
@@ -38,7 +39,7 @@ public class AffiliateManager
 	}
 
 
-	public CampaignEntity getAffiliateActiveSignupCampaign(final String affiliateName) throws Exception 
+	public CampaignEntity getAffiliateActiveSignupCampaign(final String affiliateName) 
 	{
         AffiliateEntity affiliate = getAffiliate(affiliateName);
 		
@@ -55,7 +56,10 @@ public class AffiliateManager
 		{
 			CampaignEntity ce = ace.getCampaign();
 			if( isActiveCampaign4Signup(date, ce) )
+			{
+				log.info("CampaignEntity: " + ce);
 				return ce;
+			}
 		}
 
 		return null;
--- src/main/java/com/ohadr/c3p0/leak_use_case/jobs/MyItemReader.java
+++ src/main/java/com/ohadr/c3p0/leak_use_case/jobs/MyItemReader.java
@@ -1,9 +1,11 @@
 package com.ohadr.c3p0.leak_use_case.jobs;
 
 import org.apache.log4j.Logger;
+import org.springframework.batch.core.ChunkListener;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
@@ -13,7 +15,9 @@ import com.ohadr.c3p0.leak_use_case.AffiliateManager;
 import com.ohadr.c3p0.leak_use_case.entities.AffiliateEntity;
 import com.ohadr.c3p0.leak_use_case.entities.CampaignEntity;
 
-public class MyItemReader implements ItemReader<AffiliateEntity>, StepExecutionListener
+public class MyItemReader implements ItemReader<AffiliateEntity>, 
+	StepExecutionListener,
+	ChunkListener 
 {
 	private static Logger log = Logger.getLogger(MyItemReader.class);
 	private static final String POKER_STRATEGY_AFF_NAME = "pokerStrategy";
@@ -26,16 +30,7 @@ public class MyItemReader implements ItemReader<AffiliateEntity>, StepExecutionL
 	{
 		log.info("executing reader...");
 		
-//		CampaignEntity ce = null;
-//		try
-//		{
-//			ce = affiliateManager.getAffiliateActiveSignupCampaign(POKER_STRATEGY_AFF_NAME);
-//		}
-//		catch (Exception e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+//		CampaignEntity ce = affiliateManager.getAffiliateActiveSignupCampaign(POKER_STRATEGY_AFF_NAME);
 //		log.info("CampaignEntity: " + ce);
 		return null;
 	}
@@ -43,27 +38,32 @@ public class MyItemReader implements ItemReader<AffiliateEntity>, StepExecutionL
 	@Override
 	public void beforeStep(StepExecution stepExecution)
 	{
-		log.info("executing before step...");
-		
-		CampaignEntity ce = null;
-		try
-		{
-			ce = affiliateManager.getAffiliateActiveSignupCampaign(POKER_STRATEGY_AFF_NAME);
-		}
-		catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		log.info("CampaignEntity: " + ce);
-		
+		CampaignEntity ce = affiliateManager.getAffiliateActiveSignupCampaign(POKER_STRATEGY_AFF_NAME);
 	}
 
 	@Override
 	public ExitStatus afterStep(StepExecution stepExecution)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
+
+
+	//from Spring docs: "The beforeChunk method is called after the transaction is started, but before read is called on the ItemReader"
+	@Override
+	public void beforeChunk(ChunkContext context)
+	{
+		/*
+		CampaignEntity ce = affiliateManager.getAffiliateActiveSignupCampaign(POKER_STRATEGY_AFF_NAME);
+		log.info("CampaignEntity: " + ce);
+		*/
+	}
+
+	@Override
+	public void afterChunk(ChunkContext context)
+	{}
+
+	@Override
+	public void afterChunkError(ChunkContext context)
+	{}
 
 }
